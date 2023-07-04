a71f43b7334bbcc5c5e931c115f63a9fedf9417d
Lucas-Beltran <138534254+Lucas-Beltran@users.noreply.github.com>
2023-07-04 17:11:18 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-// function appendToDisplay(value) {
-//     document.getElementById('display').value += value;
-//   }
-  
-//   function clearDisplay() {
-//     document.getElementById('display').value = '';
-//   }
-  
-//   function calculate() {
-//     var expression = document.getElementById('display').value;
-//     var result = eval(expression);
-//     document.getElementById('display').value = result;
-//   }
-
 //CALCULADORA:
 var operandoa;
 var operandob;
